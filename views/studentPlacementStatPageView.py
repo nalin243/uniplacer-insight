45d41641161e7bf8e4d89bb8711d2b252b18b203
nalin243 <devpriyanalin@gmail.com>
2024-01-23 09:05:34 +0530

    Added preliminary pie chart

--- views/studentPlacementStatPageView.py
+++ views/studentPlacementStatPageView.py
@@ -30,70 +30,58 @@ class StudentPlacementStatPageView(QMainWindow,Ui_MainWindow):
 		self.studentsNotAppliedPercent = 0
 		self.studentsPlacedPercent = 0
 
-		try:
-			self.studentsEligiblePercent = (self.viewmodel.totalEligibleStudents/self.viewmodel.totalStudents)*100
-			self.studentsAppliedPercent = (self.viewmodel.totalStudentsApplied/self.viewmodel.totalStudents)*100
-			self.studentsNotAppliedPercent = (self.viewmodel.totalStudentsNotApplied/self.viewmodel.totalStudents)*100
-			self.studentsPlacedPercent = (self.viewmodel.totalStudentsPlaced/self.viewmodel.totalStudents)*100
-		except:
-			pass
-
-
 		self.campusComboBox.currentTextChanged.connect(self.filterChanged)
 		self.batchComboBox.currentTextChanged.connect(self.filterChanged)
 		self.departmentComboBox.currentTextChanged.connect(self.filterChanged)
 		self.courseComboBox.currentTextChanged.connect(self.filterChanged)
 		self.genderComboBox.currentTextChanged.connect(self.filterChanged)
 
-		# self.data = [[' Not Applied', self.studentsNotAppliedPercent],[' Placed', self.studentsPlacedPercent],[' Eligible', self.studentsEligiblePercent],['Applied',self.studentsAppliedPercent]]
-		# self.initPieChart(self.data)
+
+	def initPieChart(self,data):
+		self.pieChart.removeAllSeries()
+		self.series = QPieSeries()
+		self.series.setHoleSize(0.25)
+		self.series.setPieSize(0.71)
+
+		self.colors = ["#2ecc71","#e67e22","#e74c3c"]
+
+		for self.item in data:
+			self.series.append(QPieSlice(self.item[0],self.item[1]))
+
+		for index,self.slice in enumerate(self.series.slices()):
+
+			self.color = QColor(self.colors[index])
+
+			self.slice.setLabel("{:.2f}%".format(round(self.slice.percentage()*100),2))
+			self.slice.setLabelBrush(self.color)
+			self.slice.setColor(self.color)
+			self.slice.setBorderWidth(5)
+			self.color.setAlpha(100)
+			self.slice.setBorderColor(self.color)
+			self.slice.setExplodeDistanceFactor(0.001)
+			self.slice.setExploded(True)
+			self.slice.setLabelVisible(True)
+			self.slice.setLabelFont(QFont("Serif", 11, QFont.Medium))
+			self.slice.hovered.connect(functools.partial(self.pieSliceHovered,self.slice))
+
+		self.pieChart.addSeries(self.series)
+
+		self.pieChart.legend().setFont(QFont("Serif", 20, QFont.Normal))
+		self.pieChart.legend().setMarkerShape(QLegend.MarkerShapeCircle)
+
+		for index,marker in enumerate(self.pieChart.legend().markers()):
+			marker.setLabel(self.data[index][0])
 
 
-	#TO BE USED IN ANOTHER CODE LATER
-	# def initPieChart(self,data):
-	# 	self.series = QPieSeries()
-	# 	self.series.setHoleSize(0.01)
-	# 	self.series.setPieSize(0.71)
+		self.pieChart.legend().setAlignment(Qt.AlignRight)
+		self.pieChartDisplay.setRenderHint(QPainter.Antialiasing)
 
-	# 	self.colors = ["#3498db","#e74c3c","#2ecc71","#e67e22","#9b59b6","#f1c40f"]
-
-	# 	for self.item in data:
-	# 		self.series.append(QPieSlice(self.item[0],self.item[1]))
-
-	# 	for index,self.slice in enumerate(self.series.slices()):
-
-	# 		self.color = QColor(self.colors[index])
-
-	# 		self.slice.setLabel("{:.2f}%".format(round(self.slice.percentage()*100),2))
-	# 		self.slice.setLabelBrush(self.color)
-	# 		self.slice.setColor(self.color)
-	# 		self.slice.setBorderWidth(5)
-	# 		self.color.setAlpha(100)
-	# 		self.slice.setBorderColor(self.color)
-	# 		self.slice.setExploded(True)
-	# 		self.slice.setExplodeDistanceFactor(0.05)
-	# 		self.slice.setLabelVisible(True)
-	# 		self.slice.setLabelFont(QFont("Serif", 11, QFont.Medium))
-	# 		self.slice.hovered.connect(functools.partial(self.pieSliceHovered,self.slice))
-
-	# 	self.pieChart.addSeries(self.series)
-
-	# 	self.pieChart.legend().setFont(QFont("Serif", 20, QFont.Normal))
-	# 	self.pieChart.legend().setMarkerShape(QLegend.MarkerShapeCircle)
-
-	# 	for index,marker in enumerate(self.pieChart.legend().markers()):
-	# 		marker.setLabel(self.data[index][0])
-
-
-	# 	self.pieChart.legend().setAlignment(Qt.AlignRight)
-	# 	self.pieChartDisplay.setRenderHint(QPainter.Antialiasing)
-
-	# def pieSliceHovered(self,pieSlice,state):
-	# 	self.pieChart.zoom(2.0)
-	# 	if(state):
-	# 		pieSlice.setExplodeDistanceFactor(0.1)
-	# 	else:
-	# 		pieSlice.setExplodeDistanceFactor(0.05)
+	def pieSliceHovered(self,pieSlice,state):
+		self.pieChart.zoom(2.0)
+		if(state):
+			pieSlice.setExplodeDistanceFactor(0.1)
+		else:
+			pieSlice.setExplodeDistanceFactor(0.001)
 
 
 	def show(self):
@@ -106,6 +94,9 @@ class StudentPlacementStatPageView(QMainWindow,Ui_MainWindow):
 		self.studentsNotEnrolledNumber.setText(str(self.viewmodel.getStudentAggregates()[2]))
 		self.totalDisqualifiedNumber.setText(str(self.viewmodel.getStudentAggregates()[5]))
 		self.studentsPlacedNumber.setText(str(self.viewmodel.getStudentAggregates()[3]))
+
+		self.data = [['Enrolled', self.viewmodel.totalEnrolledStudents],['Not Enrolled', self.viewmodel.totalNotEnrolledStudents],['Removed/Blocked',self.viewmodel.totalDisqualified]]
+		self.initPieChart(self.data)
 
 		super().show()	
 
@@ -124,3 +115,6 @@ class StudentPlacementStatPageView(QMainWindow,Ui_MainWindow):
 		self.studentsNotEnrolledNumber.setText(str(self.viewmodel.getStudentAggregates()[2]))
 		self.totalDisqualifiedNumber.setText(str(self.viewmodel.getStudentAggregates()[5]))
 		self.studentsPlacedNumber.setText(str(self.viewmodel.getStudentAggregates()[3]))
+
+		self.data = [['Enrolled', self.viewmodel.totalEnrolledStudents],['Not Enrolled', self.viewmodel.totalNotEnrolledStudents],['Removed/Blocked',self.viewmodel.totalDisqualified]]
+		self.initPieChart(self.data)
